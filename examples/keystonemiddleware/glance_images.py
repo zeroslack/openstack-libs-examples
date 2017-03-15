df8ea5c29e4e08dc1af4305d2ebe63cdcbd9454b
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-03-15 20:14:14 +0000

    WIP: quick take

--- examples/keystonemiddleware/glance_images.py
+++ examples/keystonemiddleware/glance_images.py
@@ -8,30 +8,43 @@ from keystonemiddleware import auth_token
 from oslo_config import cfg
 import webob.dec
 from wsgiref import simple_server
+import os
+import sys
 
+CONF = cfg.CONF
+CONF(project='testservice')
+session.Session.register_conf_options(cfg.CONF, 'communication')
+vars = filter(lambda x: x[0].startswith('OS_'), os.environ.iteritems())
+conf_keys = CONF.keys()
+for k, v in vars:
+# Try the full var first
+    n = k.lower()
+    cands = (n, n[3:])
+    for var in cands:
+        if var in conf_keys:
+            self.conf.set_default(name=var, default=v)
+            break
+
+CONF(sys.argv[1:])
 
 # TODO: --help doesn't seem to work either way...
-cfg.CONF(project='testservice')
-session.Session.register_conf_options(cfg.CONF, 'communication')
 SESSION = session.Session.load_from_conf_options(cfg.CONF, 'communication')
 
 @webob.dec.wsgify
 def app(req):
+    print(Exception(req.environ['keystone.token_auth']._auth.__dict__))
     glance = client.Client('2',
                            session=SESSION,
                            auth=req.environ['keystone.token_auth'])
 
     return webob.Response(json.dumps([i.name for i in glance.images.list()]))
 
-
-app = auth_token.AuthProtocol(app, {})
-server = simple_server.make_server('', 8000, app)
-#server.serve_forever()
-
 if __name__ == '__main__':
     import logging
     import sys
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logger = logging.getLogger(cfg.CONF.project)
-    cfg.CONF.log_opt_values(cfg.LOG, cfg.logging.INFO)
+
+    app = auth_token.AuthProtocol(app,{})
+    server = simple_server.make_server('', 8000, app)
     server.serve_forever()
