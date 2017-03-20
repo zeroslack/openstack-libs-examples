48c9297a90246a6a5357b605bb8539007b6524bf
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-03-20 21:06:46 +0000

    Use auth plugin directly from req.environ

--- examples/keystonemiddleware/glance_images.py
+++ examples/keystonemiddleware/glance_images.py
@@ -3,7 +3,8 @@
 # http://www.jamielennox.net/blog/2015/09/10/user-auth-in-openstack-services
 from glanceclient import client
 import json
-from keystoneclient import session
+from keystoneauth1 import loading
+from keystoneauth1 import session
 from keystoneclient import client as ks_client
 from keystonemiddleware import auth_token
 from oslo_config import cfg
@@ -14,7 +15,7 @@ import sys
 
 CONF = cfg.CONF
 CONF(project='testservice')
-session.Session.register_conf_options(cfg.CONF, 'communication')
+loading.session.register_conf_options(cfg.CONF, 'communication')
 vars = filter(lambda x: x[0].startswith('OS_'), os.environ.iteritems())
 conf_keys = CONF.keys()
 for k, v in vars:
@@ -29,24 +30,30 @@ for k, v in vars:
 CONF(sys.argv[1:])
 
 # TODO: --help doesn't seem to work either way...
-SESSION = session.Session.load_from_conf_options(cfg.CONF, 'communication')
+# Doesn't seem to matter whether keystoneclient.session.Session or keystoneauth1.session.Session
+SESSION = loading.session.load_from_conf_options(cfg.CONF, 'communication')
 
 @webob.dec.wsgify
 def app(req):
 # N.B. below does not work
 # 
 # DiscoveryFailure: Not enough information to determine URL. Provide either a Session, or auth_url or endpoint
-#    kclient = ks_client.Client('3',
-#                               session=SESSION,
-#                               auth=req.environ['keystone.token_auth'])
+# Loaded session will not set 'auth' attribute
+    SESSION.auth = req.environ['keystone.token_auth']._auth
+    kclient = ks_client.Client('3', session=SESSION)
+    projects = kclient.projects.list()
 
+    # Clear the auth added above to demonstrate glanceclient 'auth' param
+    session.auth = None
 # TODO(kamidzi): Need to handle InvalidToken exception
+
     glance = client.Client('2',
                            session=SESSION,
                            auth=req.environ['keystone.token_auth'])
 
     resp = {
-        'glance_images': [i.name for i in glance.images.list()],
+        'keystoneclient.projects': [p.name for p in projects],
+        'glanceclient.images': [i.name for i in glance.images.list()],
         'keystone.token_auth.user': req.environ['keystone.token_auth'].user._data,
     }
 
@@ -60,4 +67,5 @@ if __name__ == '__main__':
 
     app = auth_token.AuthProtocol(app,{})
     server = simple_server.make_server('', 8000, app)
+    server.handle_request()
     server.serve_forever()
